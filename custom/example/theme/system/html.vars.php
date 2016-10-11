b909da89b3252597d6ec84e4e4f19a1b5e492218
Artemnsk <shelkov1991@gmail.com>
2016-10-11 15:02:23 +0700

    Make example custom theme files.

---
+++ custom/example/theme/system/html.vars.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * html.vars.php
+ *
+ * @see html.tpl.php
+ */
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function himss17_preprocess_html(&$variables) {
+	// Default implementation.
+	_default_himss17_preprocess_html($variables);
+	// Some additional overrides go next.
+	// ...
+}
